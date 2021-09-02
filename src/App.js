1371202a6890790b57fdc868586c0acc1df7cc20
gstafylopatis <gstafylopatis@gmail.com>
2021-09-02 17:10:34 +0300

    Finished Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+import React from 'react';
+import {Route, Redirect, Switch} from 'react-router-dom';
+
 import Movies from './components/movies';
+import NavBar from './components/navBar';
+import notFound from './components/notFound';
+import Rentals from './components/rentals';
+import Customers from './components/customers';
+import MovieForm from './components/movieForm';
+
+import './App.css';
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container-fluid">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={notFound} />
+          <Redirect from="/" to="/movies" exact />
+          <Redirect to="not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
